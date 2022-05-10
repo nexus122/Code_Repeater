5fa37c14d47d5c71b106211d59c2e3f98a19f678
nexus122 <juan-pablo122@hotmail.es>
2022-05-10 12:02:07 +0200

    Code Repeater

---
+++ script.js
@@ -0,0 +1,24 @@
+let phraseInput = document.getElementById('phrase');
+let iterations = document.getElementById('number');
+let btn = document.getElementById('enviar');
+
+phraseInput.addEventListener('change', function(e){
+    phraseInput.value = phraseInput.value.replaceAll('`', '');
+    phraseInput.value = '`'+phraseInput.value+'`';
+})
+
+btn.addEventListener('click', function(){
+    let phrase = phraseInput.value;
+    phrase = phrase.replaceAll('x', '${i}');
+    console.log(phrase);
+
+    let code = "";
+    console.log(phrase);
+
+    for(var b = 0; b < iterations.value; b++){
+        let i = b +1;        
+        code += eval(phrase)+'\n';
+    }
+
+    document.querySelector("textarea").value = code;
+})
